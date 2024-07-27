3e4bb3ed417fa0b2fa54818ada0554c8f2f4ebf5
Kaden <kadenw258@gmail.com>
2024-07-27 07:03:34 -0500

    Config
    Fixed some config and background issues

--- client/ToDoApp/src/main.jsx
+++ client/ToDoApp/src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// import ToDoCreater from "./ToDoCreater";
-import Nav from "./Navigator";
-// import Container from "./TodoContainer.jsx";
-import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import ToDoCreater from "./ToDoCreater";
+import Container from "./TodoContainer.jsx";
+
+import Nav from "./Navigator";
+import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Nav />
